27fe3838f5e04f3a57570298d2895980008f9f9c
meron847 <belachewmeron15@gmail.com>
2025-08-19 17:03:48 +0100

    pure componenr

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+//import { useState } from 'react'
+import React from 'react'
+import Message from './assets/component/Message';
+function App() {
+  
+  return (
+    <div>
+      <Message/>
+      <Message/>
+      <Message/>
+    </div>
+  )
+}
+
+export default App;
+    
+
+
