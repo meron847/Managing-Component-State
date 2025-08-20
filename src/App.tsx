fc43415acf34c204316c91b32668a39b647f9432
meron847 <belachewmeron15@gmail.com>
2025-08-20 16:17:46 +0100

    creating navBat and cart

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,13 @@
 import { useState } from 'react'
-//import React from 'react'
-import produce from 'immer';
-import Message from './assets/component/Message';
+import NavBar from "./assets/component/NavBar";
+import Cart from "./assets/component/Cart";
+
 function App() {
-   const [bugs, setBugs]=useState([
-    {id:1, title:'Bug 1', fixed: false},
-    {id:2, title:'Bug 2', fixed: false}
-   ])
-const handleClick=()=>{
-   setBugs(produce(draft =>{
-     const bug =draft.find(bug => bug.id === 2);
-     if(bug) bug.fixed = true;
-   }))
-};
+  const [cartItems,setCartItems]=useState(['product1', 'product2']);
   return (
     <div>
-        {bugs.map(bug=><p key={bug.title}>{bug.title} {bug.fixed ? 'Fixed' : 'New'}</p>)}
-       <button onClick={handleClick}> Click Me</button>
+       <NavBar cartItemsCount={cartItems.length}/> 
+       <Cart cartItems={cartItems} onClear={()=> setCartItems([])}/>
     </div>
   )
 }
